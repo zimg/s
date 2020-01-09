e5efc355e9086a8d0464431874572d456486bab8
yiping <dreamxyp@gmail.com>
2020-01-09 13:55:07 +0800

    moko8

--- moko8/global.js
+++ moko8/global.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 	$(".picbox div").hover(function() {
-		//$(this).animate({"top": "-350px"}, 300, "swing");  pic-list-b.png
+		// $(this).animate({"top": "-350px"}, 300, "swing");  pic-list-b.png
 		$(this).animate({"top": "-350px"}, 0, "swing");
 	},function() {
-		//$(this).stop(true,false).animate({"top": "0px"}, 300, "swing");  pic-list-b.png
+		// $(this).stop(true,false).animate({"top": "0px"}, 300, "swing");  pic-list-b.png
 		$(this).stop(true,false).animate({"top": "0px"}, 0, "swing");
 	});
 
